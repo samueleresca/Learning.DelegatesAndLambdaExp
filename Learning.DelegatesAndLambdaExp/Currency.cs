b9d7b18b2270481de588caebb08db11590782917
samueleresca <samuele.resca@gmail.com>
2015-01-22 21:37:55 +0100

    First Commit

---
+++ Learning.DelegatesAndLambdaExp/Currency.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Learning.DelegatesAndLambdaExp
+{
+    struct Currency {
+        public uint Dollars;
+        public ushort Cents;
+
+        public Currency(uint Dollars, ushort Cents) {
+            this.Dollars = Dollars;
+            this.Cents = Cents;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("${0}.{1,2:00}", Dollars, Cents);
+        }
+
+        public static string GetCurrencyUnit() {
+            return "Dollar";
+        }
+
+        public static explicit operator Currency(float value) {
+            checked {
+                uint dollars = (uint)value;
+                ushort cents = (ushort)((value - dollars) * 100);
+                return new Currency(dollars, cents);
+            }
+        }
+
+
+        public static implicit operator float(Currency value){
+            return value.Dollars + (value.Cents/100.0f);
+        }
+
+
+        public static implicit operator Currency(uint value)
+        {
+            return new Currency(value, 0);
+        }
+
+
+        public static implicit operator uint(Currency value)
+        {
+            return value.Dollars;
+        }
+    }
+}
---
+++ Learning.DelegatesAndLambdaExp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Learning.DelegatesAndLambdaExp
+{
+    class Program
+    {
+        private delegate string GetAString();
+        delegate double DoubleOp(double x);
+
+        static void Main()
+        {
+            int x = 40;
+            GetAString firstStringMethod = new GetAString(x.ToString);
+            Console.WriteLine("String is {0}", firstStringMethod());
+            //With firstStringMethod initialized to x.ToString
+            //the above steatment is equavilent to say:
+            //Console.WriteLine("String is {0}", x.ToString());
+
+            Currency balance = new Currency(34, 50);
+            
+            //firstStringMethod references an instance method
+            firstStringMethod = balance.ToString;
+            Console.WriteLine("String is {0}", firstStringMethod());
+
+            //firstStringMethod references a static method
+            firstStringMethod = new GetAString(Currency.GetCurrencyUnit);
+            Console.WriteLine("String is {0}", firstStringMethod());
+
+            /*______________________________________________________________*/
+            //You can use delegates as an array
+            DoubleOp[] operations =
+            {
+                MathOperations.MultiplyByTwo,
+                MathOperations.Square
+            };
+
+            for (int i = 0; i < operations.Length; i++) {
+                Console.WriteLine("Using operations[{0}]:", i);
+                ProcessAndDisplayNumber(operations[i], 2.0);
+                ProcessAndDisplayNumber(operations[i], 7.94);
+                ProcessAndDisplayNumber(operations[i], 1.414);
+                Console.WriteLine();
+            }
+
+
+                Console.ReadLine();
+        }
+
+        static void ProcessAndDisplayNumber(DoubleOp action, double value) {
+            double result = action(value);
+            Console.WriteLine("Value is {0}, result of operation is {1}", value, result);
+
+        }
+    }
+}
